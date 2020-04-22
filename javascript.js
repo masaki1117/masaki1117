a3d3d5f1e14cf72a094cdda12134e093e6c4b24a
masaki1117 <63140373+masaki1117@users.noreply.github.com>
2020-04-22 15:31:30 +0900

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+ document.getElementById("humburger").addEventListener("click", function() {
+  this.classList.toggle("active");
+   document.getElementById("").classList.toggle("active");
+   })
+  });
